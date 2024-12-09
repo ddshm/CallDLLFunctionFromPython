c2f9ed0f1362f89cad30cc5be6c0f0a0e0ba7e9e
Dmitry Shmelev <dshmelev@gmail.com>
2024-12-09 14:05:45 +0000

    Removed unused vars

--- MyLib/MyLib.cpp
+++ MyLib/MyLib.cpp
@@ -1,11 +1,5 @@
 #include "pch.h"
-
 #include "MyLib.h"
-
-// DLL internal state variables:
-static unsigned long long previous_;  // Previous value, if any
-static unsigned long long current_;   // Current sequence value
-static unsigned index_;               // Current seq. position
 
 double calculate(double x, double* arr, int n)
 {
@@ -15,4 +9,3 @@ double calculate(double x, double* arr, int n)
     }
     return result * x;
 }
-
